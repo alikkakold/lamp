03f30a101ba9db1f80eb70f50da8289075a4fcb1
Alikka <kotlyarenko.alina@mail.ru>
2020-06-24 14:50:00 +0300

    some features

---
+++ lamp/authapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from authapp import views
+
+app_name = 'authapp'
+urlpatterns = [
+    path('join/', views.join, name='join'),
+    path('login/', views.login, name='login')
+]
